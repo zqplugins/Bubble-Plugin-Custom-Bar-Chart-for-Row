d5f119adfd5f91cf6dcc870cd482225e6ddef8e2
zqplugins <137153217+zqplugins@users.noreply.github.com>
2023-11-10 06:18:02 +0300

    Automatic commit from Bubble

--- elements/AAC-850m6/update.js
+++ elements/AAC-850m6/update.js
@@ -1,3 +1,31 @@
 function(instance, properties, context) {
-"chart"in instance.data&&instance.data.chart.destroy();const pieId=instance.data.id,ctx=document.getElementById(pieId);var barColors=properties.barcolor.split(","),xValues=properties.bartags.split(","),yValues=properties.barvalue.split(",");const chart=new Chart(ctx,{type:"bar",data:{labels:xValues,datasets:[{backgroundColor:barColors,data:yValues}]},options:{plugins:{tooltip:properties.tooltip},hover:{mode:null},legend:{display:!1},scales:{yAxes:[{ticks:{beginAtZero:true}}]}}});instance.data.chart=chart;
+  "chart" in instance.data && instance.data.chart.destroy();
+  const pieId = instance.data.id,
+    ctx = document.getElementById(pieId);
+
+  var barColors = properties.barcolor.split(","),
+    xValues = properties.bartags.split(","),
+    yValues = properties.barvalue.split(",");
+
+  const chart = new Chart(ctx, { 
+    type: properties.chart_vertical ? "bar" : "horizontalBar", 
+    data: { labels: xValues, datasets: [{ backgroundColor: barColors, data: yValues }] }, 
+    options: { plugins: { tooltip: properties.tooltip }, 
+        responsive: true,
+        maintainAspectRatio: false,
+        indexAxis: 'y',
+        hover: { mode: null }, 
+        legend: { display: !1 }, 
+        scales: {
+            xAxes: [{ // x-axis becomes the horizontal axis
+              ticks: { beginAtZero: true }
+            }],
+            yAxes: [{ // y-axis becomes the vertical axis
+              ticks: { beginAtZero: true }
+            }]
+          }
+    } 
+
+    });
+  instance.data.chart = chart;
 }
